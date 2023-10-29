f9be5613aaae727f634e30b917fc61e590911eac
yehudit_shulevits <yehudit.shulevits@gmail.com>
2023-10-29 10:38:17 +0200

    MainScreen

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import { Routes } from 'react-router';
 import Login from './Login';
+import MainScreen from './MainScreen';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Login />} />
           <Route exact path='/Chat' element={<Chat/>} />
+          <Route exact path='/MainScreen' element={<MainScreen/>} />
         </Routes>
       </BrowserRouter>
     </div>
